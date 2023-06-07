15db2b34317e018b3b8dd3d1b6be36a6c99cf02f
Rifqi <mohamadrifki66@gmail.com>
2023-06-07 22:20:45 +0700

    c19 selesai

--- bread.js
+++ bread.js
@@ -2,13 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('node:fs');
 
-const data = [
-    { String: 'Testing Data', Integer: '12', Float: '1.45', Date: '12 Desember 2017', Boolean: 'true' },
-    { String: 'Coba Lagi', Integer: '99', Float: '100.405', Date: '20 November 2017', Boolean: 'false' },
-    { String: 'Super Sekali', Integer: '0', Float: '1.45', Date: 'kosong', Boolean: 'false' }
-
-]
-
 const app = express()
 
 let rawdata = fs.readFileSync('data.json');
@@ -16,15 +9,13 @@ let bread = JSON.parse(rawdata)
 
 const port = 3000
 
-
 app.set('view engine', 'ejs')
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
 app.use(bodyParser.json())
 
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index', { bread })
@@ -35,8 +26,8 @@ app.get('/Add', (req, res) => {
 })
 
 app.post('/Add', (req, res) => {
-    console.log(req.body.String)
-    bread.push({ "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date, "Boolean": req.body.Boolean })
+
+    bread.push({ "id": bread.length + 1, "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date || "Kosong", "Boolean": req.body.Boolean })
     fs.writeFileSync('data.json', JSON.stringify(bread, null, 4))
     res.redirect('/')
 })
@@ -44,6 +35,7 @@ app.post('/Add', (req, res) => {
 app.get('/hapus/:id', (req, res) => {
     const id = req.params.id
     bread.splice(id, 1)
+    fs.writeFileSync('data.json', JSON.stringify(bread, null, 4))
     res.redirect('/')
 })
 
@@ -54,7 +46,8 @@ app.get('/ubah/:id', (req, res) => {
 
 app.post('/ubah/:id', (req, res) => {
     const id = req.params.id
-    bread.push({ "idd": req.body.idd, "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date, "Boolean": req.body.Boolean })
+    bread[id] = ({ "id": parseInt(id) + 1, "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date, "Boolean": req.body.Boolean })
+    fs.writeFileSync('data.json', JSON.stringify(bread, null, 4))
     res.redirect('/')
 })
 
